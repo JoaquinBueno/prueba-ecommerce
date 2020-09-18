3dde63d894be700f65b4ed25d3e551bf475f2d36
JoaquinBueno <66179453+JoaquinBueno@users.noreply.github.com>
2020-09-18 13:22:24 -0300

    commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use('/products', productsRouter)
 app.use('/api/products', productsApiRouter)
 app.use('/api/auth', authApiRouter)
-
+ 
 
 //redirect
 app.get('/', function(req, res){
